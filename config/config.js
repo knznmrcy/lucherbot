7207c0e95adeaca44a80f23529e336bb480dcb00
knznmrcy <sakhaadya493@gmail.com>
2026-09-21 18:36:20 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  token: process.env.BOT_TOKEN,
+  prefix: process.env.PREFIX || '!',
+  memberRoleId: process.env.MEMBER_ROLE_ID,
+  adminRoleId: process.env.ADMIN_ROLE_ID,
+  color: {
+    primary: 0x5865F2,
+    success: 0x57F287,
+    error: 0xED4245,
+    warning: 0xFEE75C,
+  }
+};
 
